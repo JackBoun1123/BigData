1ca59a3cbf5bae519d19653637c20684e8fc9fb3
JackBoun1123 <hopboy553@gmail.com>
2025-03-29 16:47:51 +0700

    update elk

--- bigdata-main/README.md
+++
@@ -1,4 +0,0 @@
-Big Data Fundamentals Course
-by Mr. Huynh Nam
-website: www.flex.edu.vn
-email: giangdayit@gmail.com
--- cnmoi/api/test.php
+++
@@ -1,6 +0,0 @@
-<?php
-include('../class/clsSinhVien.php');
-$sinhVien =  new CNMoi();
-$result = $sinhVien->xuatSinhVien("Select * from sinhvien");
-echo $result;
-?>
